23bf30e3b2039e9052a1f07a50e8b62c8e44cf44
AxelHumbert <axelhumbert5@gmail.com>
2023-02-22 11:25:48 +0100

    EDIT: new deploy with the addition of the footer and the header of the App

---
+++ src/Footer.js
@@ -0,0 +1,22 @@
+function Footer () {
+  return (
+    <div>
+      <ul>
+        <li>
+          Contact par mail
+        </li>
+        <li>
+          <a href="https://www.facebook.com/people/Axel-Humbert/100085197099108/"><img src="../public/images/facebook.jpeg" alt="Logo Facebook" /></a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/axelhumbertpro/"><img src="../public/images/instagram.jpeg" alt="Logo Instagram" /></a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/axel-humbert-ing%C3%A9nieurs2000/"><img src="../public/images/linkedin.jpeg" alt="Logo Linkedin" /></a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div>
+      <h1>
+        Axel HUMBERT
+      </h1>
+      <img src="../public/images/cv.JPG" alt="Photo d'Axel HUMBERT" />
+    </div>
+  )
+}
+
+export default Header;
